34a96c808bf7b725e55fa1ecc8618a9e23c7274f
Denis <denisbondd111@gmail.com>
2025-02-28 21:00:04 +0300

    init

---
+++ src/main/java/com/denisbondd111/focuslinkerbackend/FocusLinkerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.denisbondd111.focuslinkerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusLinkerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FocusLinkerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/denisbondd111/focuslinkerbackend/FocusLinkerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denisbondd111.focuslinkerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusLinkerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
