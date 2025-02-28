98675a23c7d53d701dc5f3aef12799232b883e2c
Denis <denisbondd111@gmail.com>
2025-03-01 00:12:57 +0300

    feat(all): basic microservices architecture

---
+++ CalendarService/src/test/java/com/denisbondd111/calendarservice/CalendarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denisbondd111.calendarservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
